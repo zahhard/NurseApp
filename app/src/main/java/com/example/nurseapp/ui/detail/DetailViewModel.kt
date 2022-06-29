569b0c77a9020ab32f856e89930b4c41561fd0d8
zahhard <za.darardoust@gmail.com>
2022-06-29 11:12:25 +0430

    feat : get detail fun added

--- app/src/main/java/com/example/nurseapp/ui/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/nurseapp/ui/detail/DetailViewModel.kt
@@ -1,10 +1,24 @@
 package com.example.nurseapp.ui.detail
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.nurseapp.data.repository.AppRepository
+import com.example.nurseapp.model.Comment
+import com.example.nurseapp.model.Nurse
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(var appRepository: AppRepository) :ViewModel() {
+
+    var nurseItemLiveData = MutableLiveData<Nurse>()
+    var nurseCommentsLiveData = MutableLiveData<List<Comment>>()
+
+    fun getItemDetail(id: Int) {
+        viewModelScope.launch {
+            nurseItemLiveData.value = appRepository.getItemDetail(id)
+        }
+    }
 }
