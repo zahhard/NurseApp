eed0938cfde8d0bdeabf34fea828cd21c93f27bd
zahhard <za.darardoust@gmail.com>
2022-06-29 11:11:57 +0430

    feat : detail set

--- app/src/main/java/com/example/nurseapp/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/nurseapp/ui/detail/DetailFragment.kt
@@ -1,11 +1,15 @@
 package com.example.nurseapp.ui.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.nurseapp.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -27,5 +31,31 @@ class DetailFragment : Fragment() {
     ): View {
         binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var itemId = requireArguments().getInt("filmId", -1)
+        detailViewModel.getItemDetail(itemId)
+        observeProduceItem()
+    }
+
+    private fun observeProduceItem() {
+        detailViewModel.nurseItemLiveData.observe(viewLifecycleOwner) {
+            if (it != null) {
+                binding.tvDetailName.text = it.fname + " " + it.lname
+            }
+            binding.rating.text = it.average_rate.toString()
+            binding.tvCategoryList.text = it.education
+            val transformation = MultiTransformation(CenterCrop(), RoundedCorners(20))
+            Glide.with(this)
+                .load(it.image)
+                .placeholder(android.R.drawable.ic_dialog_info)
+                .error(android.R.drawable.ic_dialog_alert)
+//            .apply(RequestOptions.circleCropTransform())
+                .transform(transformation)
+                .into(binding.image)
+//                binding.detailPrice.text = it.price
+        }
     }
 }
